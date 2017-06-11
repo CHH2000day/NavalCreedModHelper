2ab97399543e568175bbf16089169bd1af397f5b
root <root@localhost>
2017-06-11 10:24:27 +0800

    Initial commit

---
+++ app/src/main/java/com/CHH2000day/mod/zjlm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.CHH2000day.mod.zjlm;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
