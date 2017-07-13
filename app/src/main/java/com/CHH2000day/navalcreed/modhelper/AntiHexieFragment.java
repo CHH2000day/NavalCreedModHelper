3d764bfcfb3642e1211662084ec30c8df4784b14
CHH2000day <chh1932383611@gmail.com>
2017-07-13 18:25:36 +0800

    反和谐功能现在可用

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/AntiHexieFragment.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/AntiHexieFragment.java
@@ -1,54 +1,122 @@
 package com.CHH2000day.navalcreed.modhelper;
 import android.os.*;
 import android.support.v4.app.*;
-import android.view.*;
-import android.widget.*;
-import android.view.View.*;
 import android.support.v7.app.*;
-import android.net.rtp.*;
-import android.support.design.widget.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import cn.bmob.v3.*;
 import java.io.*;
+import cn.bmob.v3.listener.*;
+import cn.bmob.v3.exception.*;
+import cn.bmob.v3.datatype.*;
+import android.support.design.widget.*;
 
 public class AntiHexieFragment extends Fragment
 {
-private View v;
-private Button exec;
+	private View v;
+	private Button exec;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		v=inflater.inflate(R.layout.antihexie_fragment,null);
-		exec=(Button)v.findViewById(R.id.antihexiefragmentButtonExec);
-		
+		v = inflater.inflate(R.layout.antihexie_fragment, null);
+		exec = (Button)v.findViewById(R.id.antihexiefragmentButtonExec);
+
 		exec.setOnClickListener(new OnClickListener(){
 
 				@Override
 				public void onClick(View p1)
-				{AlertDialog.Builder adb=new AlertDialog.Builder(getActivity());
-				adb.setTitle("提示")
-					.setMessage("该功能尚未实现")
-					.setPositiveButton("确定",null)
-					.create()
-					.show();
-				//throw new RuntimeException("debug");
-					
-				/*final MediaHelper mh=MediaHelper.newInstance();
-				mh.setEncodeType(MediaFormat.MIMETYPE_AUDIO_OPUS);
-				mh.setIOPath("/sdcard/nonnon.mp3","/sdcard/nonon.ogg");
-				mh.setOnCompleteListener(new MediaHelper.OnCompleteListener(){
+				{
+					Snackbar.make(v,"操作开始，在出现提示前请勿关闭程序",Snackbar.LENGTH_LONG).show();
+					String path=new StringBuilder()
+						.append(((ModHelperApplication)getActivity().getApplication()).getResFilePath())
+						.append("/files/datas")
+						.append("customnames.lua").toString();
+					final File f=new File(path);
+					if (f.exists() && f.isFile())
+					{
+						f.delete();
+					}
+					if (!f.getParentFile().exists())
+					{
+						f.getParentFile().mkdirs();
+					}
+					BmobQuery<UniversalObject> query=new BmobQuery<UniversalObject>();
+					query.getObject(StaticData.DATA_ID_ANTIHEXIE, new QueryListener<UniversalObject>(){
 
 							@Override
-							public void completed()
-							{Snackbar.make(v,"completed",Snackbar.LENGTH_LONG).show();
-							mh.release();
+							public void done(UniversalObject p1, BmobException p2)
+							{
+								final AlertDialog.Builder adb=new AlertDialog.Builder(getActivity());
+
+								if (p2 != null)
+								{
+									adb.setMessage(p2.getMessage())
+										.setTitle("失败")
+										.setPositiveButton("确定", null)
+										.create().show();
+									return;
+
+								}
+								BmobFile bf=p1.getPackagefile();
+								if (bf != null)
+								{
+									bf.download(f, new DownloadFileListener(){
+
+											@Override
+											public void done(String p1, BmobException p2)
+											{if (p2 != null)
+												{
+													adb.setTitle("错误")
+														.setMessage(p2.getMessage())
+														.setPositiveButton("确定", null)
+														.create().show();
+													return;
+												}
+												adb.setMessage("操作成功")
+													.setTitle("成功")
+													.setPositiveButton("确定", null)
+													.create().show();
+
+												// TODO: Implement this method
+											}
+
+											@Override
+											public void onProgress(Integer p1, long p2)
+											{
+												// TODO: Implement this method
+											}
+										});
+								}
 								// TODO: Implement this method
 							}
 						});
-				mh.prepare();
-				mh.startAsync();*/
+					/*AlertDialog.Builder adb=new AlertDialog.Builder(getActivity());
+					 adb.setTitle("提示")
+					 .setMessage("该功能尚未实现")
+					 .setPositiveButton("确定",null)
+					 .create()
+					 .show();*/
+					//throw new RuntimeException("debug");
+
+					/*final MediaHelper mh=MediaHelper.newInstance();
+					 mh.setEncodeType(MediaFormat.MIMETYPE_AUDIO_OPUS);
+					 mh.setIOPath("/sdcard/nonnon.mp3","/sdcard/nonon.ogg");
+					 mh.setOnCompleteListener(new MediaHelper.OnCompleteListener(){
+
+					 @Override
+					 public void completed()
+					 {Snackbar.make(v,"completed",Snackbar.LENGTH_LONG).show();
+					 mh.release();
+					 // TODO: Implement this method
+					 }
+					 });
+					 mh.prepare();
+					 mh.startAsync();*/
 					// TODO: Implement this method
 				}
 			});
-			// TODO: Implement this method
+		// TODO: Implement this method
 		return v;
 	}
 }
--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/StaticData.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/StaticData.java
@@ -1,14 +1,19 @@
 package com.CHH2000day.navalcreed.modhelper;
 
 public class StaticData
-{public static final String API_KEY="c6e9ff72dba67952e2da6ba65474cb34";
-public static final String DATAID_ALPHA="NpDx111F";
-public static final String DATAID_RELEASE="vae3AAAP";
-public static final String getDataid(){
-		if(BuildConfig.DEBUG){
+{
+	public static final String API_KEY="c6e9ff72dba67952e2da6ba65474cb34";
+	public static final String DATAID_ALPHA="NpDx111F";
+	public static final String DATAID_RELEASE="vae3AAAP";
+	public static final String DATA_ID_ANTIHEXIE="NkYO888W";
+	public static final String DATA_ID_ANNOUNCEMENT="ZeHyBBBP";
+
+	public static final String getDataid()
+	{
+		if (BuildConfig.DEBUG)
+		{
 			return DATAID_ALPHA;
 		}
 		return DATAID_RELEASE;
 	}
-	public static final String DATA_ID_ANNOUNCEMENT="ZeHyBBBP";
 }
