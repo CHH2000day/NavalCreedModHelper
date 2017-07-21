1a44fb454aea27a719de5ed4739550fc224effcd
CHH2000day <chh1932383611@gmail.com>
2017-07-21 10:22:59 +0800

    0v0

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/BGMReplacerFragment.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/BGMReplacerFragment.java
@@ -1,6 +1,17 @@
 package com.CHH2000day.navalcreed.modhelper;
 
 import android.support.v4.app.*;
+import android.os.*;
+import android.view.*;
 public class BGMReplacerFragment extends Fragment
 {
+	private View v;
+
+	@Override
+	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		return super.onCreateView ( inflater, container, savedInstanceState );
+	}
+	
 }
