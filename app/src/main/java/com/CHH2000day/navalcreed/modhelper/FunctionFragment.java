045b27fb5d6ca4b3d664b1f475e689a189f32cc2
CHH2000day <chh1932383611@gmail.com>
2017-07-24 10:34:28 +0800

    结构调整

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/FunctionFragment.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/FunctionFragment.java
@@ -1,12 +1,11 @@
 package com.CHH2000day.navalcreed.modhelper;
 import android.support.v4.app.*;
+import java.io.*;
 
 public abstract class FunctionFragment extends Fragment
 {
-	public abstract String getTargetPath()
-	public abstract boolean isHasType()
-	public abstract String getTypeName(int typenum)
-	//typenum:在Installation.manifest中定义
-	public abstract String getFileName(int num)
-	//num:在Installation.manifest中定义的文件名
+	public abstract boolean installMod(int typenum,int num,byte[] deceyptedFileData) throws IOException
+	//typenum:在Installation.manifest中的类型
+	//num:在Installation.manifest中的文件名
+	//返回值:true为成功
 }
