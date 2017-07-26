9002890c66664c2f09697336394ed87165b20a0f
CHH2000day <chh1932383611@gmail.com>
2017-07-26 23:02:29 +0800

    重写加密库，启用针对包名的签名验证

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/ModHelperApplication.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/ModHelperApplication.java
@@ -12,84 +12,92 @@ public class ModHelperApplication extends Application
 {
 	//never used
 	//private android.os.Handler merrmsghdl;
-private File resDir;
-private File sdcard;
-private File resfilesdir;
-private String resfilePath="";
-public static final String GAME_PKGNAME="com.loong.warship.zl";
-//public static final String GAME_PKGNAME="IARJisxjM8tihdkvzU52XrgfhNLAY1FK";
-	
-private String pkg_name=GAME_PKGNAME;
+	private File resDir;
+	private File sdcard;
+	private File resfilesdir;
+	private String resfilePath="";
+//public static final String GAME_PKGNAME="com.loong.warship.zl";
+	public static final String GAME_PKGNAME="IARJisxjM8tihdkvzU52XrgfhNLAY1FK";
+
+	private String pkg_name;
 	private boolean isMainPage=true;
 
-	public void setIsMainPage ( boolean isMainPage )
+	public void setIsMainPage (boolean isMainPage)
 	{
 		this.isMainPage = isMainPage;
 	}
 
-	public boolean isMainPage ( )
+	public boolean isMainPage ()
 	{
 		return isMainPage;
 	}
 	@Override
-	public void onCreate ( )
+	public void onCreate ()
 	{
-		Bmob.initialize(ModHelperApplication.this,StaticData.API_KEY);
-		BmobInstallation.getCurrentInstallation().save();
-		Log.i("Bmob initalized","Bmob initalized");
+		Bmob.initialize ( ModHelperApplication.this, StaticData.API_KEY );
+		BmobInstallation.getCurrentInstallation ( ).save ( );
+		Log.i ( "Bmob initalized", "Bmob initalized" );
 		try
 		{
-			UncaughtExceptionHandler.getInstance().init(ModHelperApplication.this);
+			UncaughtExceptionHandler.getInstance ( ).init ( ModHelperApplication.this );
 		}
 		catch (PackageManager.NameNotFoundException e)
 		{}
-		/*try
+		try
 		{
 			Signature s=getPackageManager ( ).getPackageInfo ( getPackageName ( ), getPackageManager ( ).GET_SIGNATURES ).signatures[ 0 ];
-			IceKeyHelper mhelper=new IceKeyHelper ( s.toByteArray(), 0 );
-			pkg_name=new String(mhelper.decrypt(Base64.decode(GAME_PKGNAME,Base64.DEFAULT)));
-			Log.e(pkg_name,pkg_name);
-			}
+			IceKeyHelper mhelper=new IceKeyHelper ( s.toByteArray ( ), 0 );
+			//pkg_name=Base64.encodeToString(mhelper.encrypt(GAME_PKGNAME.getBytes()),Base64.DEFAULT);
+			pkg_name = new String ( mhelper.decrypt ( Base64.decode ( GAME_PKGNAME, Base64.DEFAULT ) ) ).trim ( );
+
+			Log.e ( pkg_name, pkg_name );
+		}
 		catch (Exception e)
 		{}
-		*/
+
 		//加密部分存在问题,暂不实装
 		// TODO: Implement this method
 		super.onCreate ( );
 	}
 	/*public android.os.Handler getErrMsgHdl ( )
+	 {
+	 return merrmsghdl;
+	 }*/
+	public File getResDir ()
 	{
-		return merrmsghdl;
-	}*/
-	public File getResDir(){
-		if(resDir==null){
-			sdcard=Environment.getExternalStorageDirectory();
+		if (resDir == null)
+		{
+			sdcard = Environment.getExternalStorageDirectory ( );
 			//resfilePath: /sdcard/Android/data/$pkgname
-			resfilePath=new StringBuilder()
-						.append(sdcard.getAbsolutePath())
-						.append(File.separatorChar)
-						.append("Android")
-						.append(File.separatorChar)
-						.append("data")
-						.append(File.separatorChar)
-						.append(pkg_name)
-						.toString();
-		resDir=new File(resfilePath);
-			
+			resfilePath = new StringBuilder ( )
+				.append ( sdcard.getAbsolutePath ( ) )
+				.append ( File.separatorChar )
+				.append ( "Android" )
+				.append ( File.separatorChar )
+				.append ( "data" )
+				.append ( File.separatorChar )
+				.append ( pkg_name )
+				.toString ( );
+			resDir = new File ( resfilePath );
+
 		}
 		return resDir;
 	}
-	public String getResPath(){
-		return getResDir().getAbsolutePath();
+	public String getResPath ()
+	{
+		return getResDir ( ).getAbsolutePath ( );
 	}
-	public File getResFilesDir(){
-		if(resfilesdir==null){
-			resfilesdir=new File(getResDir(),"files");
+	public File getResFilesDir ()
+	{
+		if (resfilesdir == null)
+		{
+			resfilesdir = new File ( getResDir ( ), "files" );
 		}
 		return resfilesdir;
 	}
-	public String getResFilesDirPath(){
-		return getResFilesDir().getAbsolutePath();
+	public String getResFilesDirPath ()
+	{
+		return getResFilesDir ( ).getAbsolutePath ( );
 	}
-	
+
 }
