521244becc290620ab0678d21524b4bb3e73db5f
CHH2000day <chh1932383611@gmail.com>
2017-07-26 12:02:49 +0800

    完成BGMReplacerFragment的获取文件部分

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/BGMReplacerFragment.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/BGMReplacerFragment.java
@@ -18,14 +18,14 @@ public class BGMReplacerFragment extends FunctionFragment
 	private static final int MUSICCOUNT_BATTLEEND=3;
 	private static final int MUSICCOUNT_BATTLEVICTORY=4;
 	private static final int MUSICCOUNT_BATTLEFAIL=2;
-	private static final String[] SCENE={};
-	private static final String[] SCENE_TOSHOW={};
+	private static final String[] SCENE={"Harbor","BattleStart","BattleHeat","BattleEnd","Victory","Fial"/*因为你游程序员把Fail打成Fial了，所以将错就错了*/,"Loading"};
+	private static final String[] SCENE_TOSHOW={"港口","战斗开始","战斗激战","战斗即将结束","战斗胜利","战斗失败","加载音乐"};
 	private static final String[] FILENAMES_UNIVERSAL={"1","2","3","4","5","6","7"};
 	private static final String[] FILENAMES_UNSELECTED={"请选择情景"};
-	private static final String[] FILENAMES_BATTLEFAIL={};
-	private static final String[] FILENAMES_BATTLEFAIL_TOSHOW={};
-	private static final String[] FILENAMES_LOADING={};
-	private static final String[] FILENAMES_LOADING_TOSHOW={};
+	private static final String[] FILENAMES_BATTLEFAIL={"Danger","Fail"};
+	private static final String[] FILENAMES_BATTLEFAIL_TOSHOW={"即将失败","失败"};
+	private static final String[] FILENAMES_LOADING={"Loading","Login","Queuing"};
+	private static final String[] FILENAMES_LOADING_TOSHOW={"加载中","登录","匹配中"};
 	public static final int TYPE_HARBOR=10;
 	public static final int TYPE_LOADING=11;
 	public static final int TYPE_BATTLESTART=12;
@@ -33,13 +33,15 @@ public class BGMReplacerFragment extends FunctionFragment
 	public static final int TYPE_BATTLEEND=14;
 	public static final int TYPE_BATTLEVICTORY=15;
 	public static final int TYPE_BATTLEFAIL=16;
-	
-	
+	public static final String FORMAT_OGG=".ogg";
+	public static final String FORMAT_WAV=".wav";
+
+
 	private ModHelperApplication mapplication;
 	private FileNameAdapter mfilenameadapter;
 	private View v;
 	private Spinner mSceneSpinner,mFileNameSpinner;
-	
+
 
 
 
@@ -50,7 +52,7 @@ public class BGMReplacerFragment extends FunctionFragment
 		// TODO: Implement this method
 		v = inflater.inflate ( R.layout.bgmreplacer, null );
 		//初始化文件名的适配器
-		mfilenameadapter=new FileNameAdapter(getActivity(),android.R.layout.simple_spinner_dropdown_item,FILENAMES_UNSELECTED);
+		mfilenameadapter = new FileNameAdapter ( getActivity ( ), android.R.layout.simple_spinner_dropdown_item, FILENAMES_UNSELECTED );
 		mapplication = (ModHelperApplication)getActivity ( ).getApplication ( );
 		return v;
 	}
@@ -63,12 +65,6 @@ public class BGMReplacerFragment extends FunctionFragment
 	}
 	private File getTargetFile (int type, int num, String format)
 	{
-		String s=getFileName ( type, num );
-		if (s == null)
-		{
-			Log.e ( "BGMreplacer", "Failed to get target filename" );
-			return null;
-		}
 		File f=new File (
 			new StringBuilder ( )
 			.append ( mapplication.getResFilesDirPath ( ) )
@@ -79,48 +75,84 @@ public class BGMReplacerFragment extends FunctionFragment
 			.append ( File.separatorChar )
 			.append ( SCENE[ type ] )
 			.append ( File.separatorChar )
-			.append ( s )
+			.append ( getFileName ( type, num ) )
 			.append ( format )
 			.toString ( )
 		);
 		return f;
 	}
-	String getFileName (int type, int numq)
+	String getFileName (int type, int num)
 	{
-		return null;
+		return getFileNameStrings ( type )[ num ];
 	}
-	private static String[] getFileNameStringsToShow(int type){
-		
-		return null;
+	private static String[] getFileNameStringsToShow (int type)
+	{
+		if (type == TYPE_BATTLEFAIL)
+		{
+			return FILENAMES_BATTLEFAIL_TOSHOW;
+		}
+		if (type == TYPE_LOADING)
+		{
+			return FILENAMES_LOADING_TOSHOW;
+		}
+		return getFileNameStrings ( type );
 	}
-	private static String[] getFileNameStrings(int type){
-		return null;
+	private static String[] getFileNameStrings (int type)
+	{
+		if (type == TYPE_BATTLEFAIL)
+		{
+			return FILENAMES_BATTLEFAIL;
+		}
+		if (type == TYPE_LOADING)
+		{
+			return FILENAMES_LOADING;
+		}
+		int count=0;
+		switch (type)
+		{
+			case TYPE_HARBOR:
+				count = MUSICCOUNT_HARBOR;
+				break;
+			case TYPE_BATTLESTART:
+				count = MUSICCOUNT_BATTLESTART;
+				break;
+			case TYPE_BATTLEHEAT:
+				count = MUSICCOUNT_BATTLEHEAT;
+				break;
+			case TYPE_BATTLEEND:
+				count = MUSICCOUNT_BATTLEEND;
+				break;
+			case TYPE_BATTLEVICTORY:
+				count = MUSICCOUNT_BATTLEVICTORY;
+				break;
+		}
+		return Arrays.copyOf ( FILENAMES_UNIVERSAL, count );
 	}
-	
+
 	private static class FileNameAdapter extends ArrayAdapter<String>
 	{
 
 		private static String[] data;
 		private static String[] act_data;
 		private static FileNameAdapter self;
-		public static FileNameAdapter getInstance(Context context,int textViewResId,int type)
+		public static FileNameAdapter getInstance (Context context, int textViewResId, int type)
 		{
-			data=getFileNameStringsToShow(type);
-			act_data=getFileNameStrings(type);
-			self=new FileNameAdapter(context,textViewResId,data);
+			data = getFileNameStringsToShow ( type );
+			act_data = getFileNameStrings ( type );
+			self = new FileNameAdapter ( context, textViewResId, data );
 			return self;
 		}
 		private FileNameAdapter (Context context, int textViewResourceId, String[] data)
 		{
 			super ( context, textViewResourceId, data );
 		}
-		public void reconfigure(int type)
+		public void reconfigure (int type)
 		{
-			this.clear();
-			this.data=getFileNameStringsToShow(type);
-			this.act_data=getFileNameStrings(type);
+			this.clear ( );
+			this.data = getFileNameStringsToShow ( type );
+			this.act_data = getFileNameStrings ( type );
 		}
-		public String[] getCurrentData()
+		public String[] getCurrentData ()
 		{
 			return this.act_data;
 		}
