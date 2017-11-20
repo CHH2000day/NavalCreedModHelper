572d01986a9629c910f0766f7dbd306a048f9663
CHH2000day <chh1932383611@gmail.com>
2017-11-20 22:34:22 +0800

    卫星升空

---
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/LoginMovieReplacer.java
@@ -0,0 +1,16 @@
+package com.CHH2000day.navalcreed.modhelper;
+import android.support.v4.app.*;
+import android.view.*;
+import android.os.*;
+
+public class LoginMovieReplacer extends Fragment
+{
+
+	@Override
+	public View onCreateView ( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState )
+	{
+		// TODO: Implement this method
+		return super.onCreateView ( inflater, container, savedInstanceState );
+	}
+	
+}
