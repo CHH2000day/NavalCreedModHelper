dffcdaec0180c6b171273e451da962814d7c0ead
CHH2000day <chh1932383611@gmail.com>
2018-01-31 10:46:12 +0800

    Mod包安装器部分逻辑完成

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/ModPackageInstallerFragment.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/ModPackageInstallerFragment.java
@@ -4,10 +4,16 @@ import android.os.*;
 import android.view.*;
 import java.io.*;
 import android.net.*;
+import android.app.Activity;
+import android.content.*;
+import com.CHH2000day.navalcreed.modhelper.ModPackageInfo.*;
+import org.json.*;
 
 public class ModPackageInstallerFragment extends Fragment
 {
 
+	private UriLoader loader;
+	private ModPackageInstallHelper mpih;
 	@Override
 	public void onCreate ( Bundle savedInstanceState )
 	{
@@ -22,9 +28,55 @@ public class ModPackageInstallerFragment extends Fragment
 		return inflater.inflate ( R.layout.modinfopage, null );
 	}
 
-	public void selectFile ( Uri uri )
+	@Override
+	public void onActivityCreated ( Bundle savedInstanceState )
+	{
+		// TODO: Implement this method
+		if ( loader != null )
+		{
+			selectFile ( loader.getUri ( ) );
+			//注销接口防止被重复使用
+			loader = null;
+		}
+	}
+
+
+	@Override
+	public void onAttach ( Activity activity )
 	{
 
+		// TODO: Implement this method
+		super.onAttach ( activity );
+		if ( activity != null )
+		{
+			loader = (UriLoader)activity;
+		}
+	}
+
+
+	public void selectFile ( Uri uri )
+	{
+		String filepath=Utils.resolveFilePath ( uri );
+		try
+		{
+			mpih = new ModPackageInstallHelper ( new File ( filepath ), (Main)getActivity ( )  );
+		}
+		catch (IOException e)
+		{}
+		catch (ModPackageInfo.IllegalModInfoException e)
+		{}
+
+	}
+	public static interface UriLoader
+	{
+		public Uri getUri ( );
+	}
+
+	@Override
+	public void onActivityResult ( int requestCode, int resultCode, Intent data )
+	{
+		// TODO: Implement this method
+		super.onActivityResult ( requestCode, resultCode, data );
 	}
 
 }
