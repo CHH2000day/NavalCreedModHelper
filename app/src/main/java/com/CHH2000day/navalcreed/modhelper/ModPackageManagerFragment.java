3b453106fa5659aa4a9304f05eac12178767e017
CHH2000day <chh1932383611@gmail.com>
2018-02-02 16:55:08 +0800

    mod包管理器UI实现

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/ModPackageManagerFragment.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/ModPackageManagerFragment.java
@@ -8,11 +8,15 @@ import android.content.res.*;
 import android.widget.CompoundButton.*;
 import android.support.v7.app.*;
 import android.content.*;
+import java.util.*;
+import android.support.design.widget.*;
 
-public class ModPackageManagerFragment extends Fragment
+public class ModPackageManagerFragment extends Fragment implements ModPackageManager.OnDataChangedListener
 {
+
 	private View v;
 	private RecyclerView recyclerview;
+	private MyAdapter adapter;
 	private ToggleButton ovrd_switch;
 
 
@@ -38,6 +42,7 @@ public class ModPackageManagerFragment extends Fragment
 		{
 			ovrd_switch.setChecked ( true );
 		}
+		adapter = new MyAdapter ( getActivity ( ) );
 		ovrd_switch.setOnCheckedChangeListener ( new OnCheckedChangeListener ( ){
 
 				@Override
@@ -92,7 +97,169 @@ public class ModPackageManagerFragment extends Fragment
 					}
 				}
 			} );
+		if ( !ModPackageManager.getInstance ( ).isOverride ( ) )
+		{
+			recyclerview.setLayoutManager ( new LinearLayoutManager ( getActivity ( ), LinearLayoutManager.VERTICAL, false ) );
+			recyclerview.setAdapter ( adapter );
+		}
+	}
+
+	@Override
+	public void onResume ( )
+	{
+		// TODO: Implement this method
+		super.onResume ( );
+		ModPackageManager.getInstance ( ).setonDataChangedListener ( this );
+		onChange ( );
+	}
+
+	@Override
+	public void onPause ( )
+	{
+		// TODO: Implement this method
+		super.onPause ( );
+		ModPackageManager.getInstance ( ).unregistDataChangeListener ( );
+	}
+
+	@Override
+	public void onChange ( )
+	{	if ( adapter != null && recyclerview != null )
+		{
+			adapter = null;
+			adapter = new MyAdapter ( getActivity ( ) );	
+			recyclerview.setAdapter ( adapter );
+		}
+		// TODO: Implement this method
 	}
 
 
+	private class MyAdapter extends RecyclerView.Adapter
+	{
+
+		private Context context;
+		private LayoutInflater li;
+		private String[] keys;
+		private OnLongClickListener listener;
+		public MyAdapter ( Context ctx )
+		{
+			context = ctx;
+			li = LayoutInflater.from ( context );
+			keys = ModPackageManager.PUBLIC_KEYS;
+			listener = new UninstallListener ( );
+		}
+
+		@Override
+		public RecyclerView.ViewHolder onCreateViewHolder ( ViewGroup p1, int p2 )
+		{
+			View vi=li.inflate ( R.layout.modmanager_item, null );
+			// TODO: Implement this method
+			return new ViewHolder ( vi );
+		}
+
+		@Override
+		public void onBindViewHolder ( RecyclerView.ViewHolder p1, int p2 )
+		{View v=( (ViewHolder)p1 ).getView ( );
+			RelativeLayout rl=(RelativeLayout)v.findViewById ( R.id.modmanageritemRelativeLayout );
+			TextView info=(TextView)v.findViewById ( R.id.modmanageritemTextView );
+			TextView memo=(TextView)v.findViewById ( R.id.modmanageritemMemo );
+			rl.setTag ( p2 );
+			//如果对应mod包已安装
+			if ( !ModPackageManager.getInstance ( ).getModList ( ).get ( keys [ p2 ] ).equals ( "" ) )
+			{
+				info.setText ( new StringBuilder ( ).append ( "mod类型:" )
+							  .append ( ModPackageManager.resolveModType ( keys [ p2 ] ) )
+							  .append ( "\n" )
+							  .append ( ModPackageManager.getInstance ( ).getModList ( ).get ( keys [ p2 ] ).toString ( ) ) );
+				rl.setOnLongClickListener ( listener );
+
+
+			}
+			else
+			{
+				memo.setText ( "" );
+				info.setText ( new StringBuilder ( ).append ( "mod类型:" )
+							  .append ( ModPackageManager.resolveModType ( keys [ p2 ] ) )
+							  .append ( "\n" )
+							  .append ( "mod未安装" ).toString ( ) );
+			}
+
+
+			// TODO: Implement this method
+		}
+
+		@Override
+		public int getItemCount ( )
+		{
+			// TODO: Implement this method
+			return keys.length;
+		}
+
+
+
+
+	}
+	private class UninstallListener implements OnLongClickListener
+	{
+
+		@Override
+		public boolean onLongClick ( View p1 )
+		{
+			int num=p1.getTag ( );
+			String key=ModPackageManager.PUBLIC_KEYS [ num ];
+			if ( key.equals ( ModPackageInfo.SUB_MODTYPE_CV_CN ) || key.equals ( ModPackageInfo.SUB_MODTYPE_CV_EN ) )
+			{
+				if ( ModPackageManager.getInstance ( ).checkInstalled ( ModPackageInfo.MODTYPE_CV, key ) )
+				{
+					uninstall ( key );
+				}
+			}
+			else
+			{
+				if ( ModPackageManager.getInstance ( ).checkInstalled ( key, ModPackageInfo.SUBTYPE_EMPTY ) )
+				{
+					uninstall ( key );
+				}
+			}
+
+
+			// TODO: Implement this method
+			return true;
+		}
+		private void uninstall ( final String key )
+		{
+			AlertDialog.Builder adb=new AlertDialog.Builder ( getActivity ( ) );
+			adb.setTitle ( "注意" )
+				.setMessage ( "确定要卸载" + ModPackageManager.resolveModType ( key ) + ":" + ModPackageManager.getInstance ( ).getModList ( ).get ( key ) + "么?" )
+				.setNegativeButton ( "取消", null )
+				.setPositiveButton ( "确定", new DialogInterface.OnClickListener ( ){
+
+					@Override
+					public void onClick ( DialogInterface p1, int p2 )
+					{
+						String type=	key.equals ( ModPackageInfo.SUB_MODTYPE_CV_CN ) || key.equals ( ModPackageInfo.SUB_MODTYPE_CV_EN ) ?ModPackageInfo.MODTYPE_CV: key;
+						String subType=type.equals ( ModPackageInfo.MODTYPE_CV ) ?key: ModPackageInfo.SUBTYPE_EMPTY;
+						ModPackageManager.getInstance ( ).requestUninstall ( type, subType , (ModHelperApplication)getActivity ( ).getApplication ( ) );
+						Snackbar.make ( v, "操作完成", Snackbar.LENGTH_LONG ).show ( );
+						// TODO: Implement this method
+					}
+				} );
+			adb.create ( ).show ( );
+
+		}
+
+	}
+	private class ViewHolder extends RecyclerView.ViewHolder
+	{
+
+		private View v;
+		public ViewHolder ( View v )
+		{
+			super ( v );
+			this.v = v;
+		}
+		public View getView ( )
+		{
+			return v;
+		}
+	}
 }
