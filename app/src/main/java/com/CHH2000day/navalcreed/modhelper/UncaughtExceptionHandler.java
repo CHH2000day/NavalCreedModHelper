fe65be5850e98e982da33c2753a881bd3e5302db
CHH2000day <chh1932383611@gmail.com>
2018-07-07 14:22:32 +0800

    Updated bug report

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/UncaughtExceptionHandler.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/UncaughtExceptionHandler.java
@@ -19,82 +19,82 @@ public class UncaughtExceptionHandler implements Thread.UncaughtExceptionHandler
 	private static final String PREF_NAME="bugly";
 	private static final String ERRTIME="errtime";
 	private static UncaughtExceptionHandler exceptionHandler;
-	private UncaughtExceptionHandler ( )
+	private UncaughtExceptionHandler()
 	{}
-	public static UncaughtExceptionHandler getInstance ( )
+	public static UncaughtExceptionHandler getInstance()
 	{
-		if ( exceptionHandler == null )
-			exceptionHandler = new UncaughtExceptionHandler ( );
+		if (exceptionHandler == null)
+			exceptionHandler = new UncaughtExceptionHandler();
 		return exceptionHandler;
 	}
 	private boolean inited=false;
 	private Context ctx,ce;
 	private Thread.UncaughtExceptionHandler defaultHandler;
-	public void init ( Context ctx ) throws PackageManager.NameNotFoundException
+	public void init(Context ctx) throws PackageManager.NameNotFoundException
 	{   ce = ctx;
-		if ( inited )return;
+		if (inited)return;
 		this.ctx = ctx;
-		defaultHandler = Thread.getDefaultUncaughtExceptionHandler ( );
-		Thread.setDefaultUncaughtExceptionHandler ( this );
-		app_ver = ctx.getPackageManager ( ).getPackageInfo ( ctx.getPackageName ( ), 0 ).versionName;
+		defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+		Thread.setDefaultUncaughtExceptionHandler(this);
+		app_ver = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0).versionName;
 
-		app_ver_int = ctx.getPackageManager ( ).getPackageInfo ( ctx.getPackageName ( ), 0 ).versionCode;
-		parent = ctx.getExternalFilesDir ( "err-log" );
+		app_ver_int = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0).versionCode;
+		parent = ctx.getExternalFilesDir("err-log");
 
-		checkUnpostedBug ( );
+		checkUnpostedBug();
 
 	}
 
-	public void checkUnpostedBug ( )
+	public void checkUnpostedBug()
 	{
-		final long time=ctx.getSharedPreferences ( PREF_NAME, 0 ).getLong ( ERRTIME, -1 );
-		if ( time == -1 )
+		final long time=ctx.getSharedPreferences(PREF_NAME, 0).getLong(ERRTIME, -1);
+		if (time == -1)
 		{return;}
 		else
 		{
-			Log.d ( "Bugly", "Post unposted log" );
-			Toast.makeText ( ctx, "提交之前错误日志中", Toast.LENGTH_LONG ).show ( );
-			final File tgt=new File ( parent, new StringBuilder ( ).append ( time ).append ( ".log" ).toString ( ) );
+			Log.d("Bugly", "Post unposted log");
+			Toast.makeText(ctx, "提交之前错误日志中", Toast.LENGTH_LONG).show();
+			final File tgt=new File(parent, new StringBuilder().append(time).append(".log").toString());
 			try
 			{
-				FileInputStream fis =new FileInputStream ( tgt );
-				String testerid=ctx.getSharedPreferences ( PREF_NAME, 0 ).getString ( StaticData.KEY_TESTER_ID, "" );
-				new Bugly ( ).postBug ( testerid + "\n" + new String ( Utils.readAllbytes ( fis ) ), time, app_ver, app_ver_int ).save ( new SaveListener<String> ( ){
+				FileInputStream fis =new FileInputStream(tgt);
+				String testerid=ctx.getSharedPreferences(PREF_NAME, 0).getString(StaticData.KEY_TESTER_ID, "");
+				new Bugly().postBug(testerid + "\n" + new String(Utils.readAllbytes(fis)), time, app_ver, app_ver_int).save(new SaveListener<String>(){
 
 						@Override
-						public void done ( String p1, BmobException p2 )
+						public void done(String p1, BmobException p2)
 						{
-							if ( p2 == null )
+							if (p2 == null)
 							{
-								ctx.getSharedPreferences ( PREF_NAME, 0 ).edit ( ).putLong ( ERRTIME, -1 ).commit ( );
+								ctx.getSharedPreferences(PREF_NAME, 0).edit().putLong(ERRTIME, -1).commit();
 							}
 							else
-							{p2.printStackTrace ( );}
+							{p2.printStackTrace();}
 							// TODO: Implement this method
 						}
-					} );}
+					});}
 			catch (Exception e)
 			{
-				e.printStackTrace ( );
+				e.printStackTrace();
 			}
 
 		}
 	}
 	@Override
-	public void uncaughtException ( Thread p1, Throwable p2 )
+	public void uncaughtException(Thread p1, Throwable p2)
 	{
 
 //p1:出错线程
 //p2:错误
 
-		final StringWriter stringWriter=new StringWriter ( );
-		PrintWriter printWriter=new PrintWriter ( stringWriter );
-		p2.printStackTrace ( printWriter );
+		final StringWriter stringWriter=new StringWriter();
+		PrintWriter printWriter=new PrintWriter(stringWriter);
+		p2.printStackTrace(printWriter);
 //保存错误
-		final long time=System.currentTimeMillis ( );
+		final long time=System.currentTimeMillis();
 
-		String filename=String.valueOf ( time ) + ".log";
-		final File file=new File ( parent, filename );
+		String filename=String.valueOf(time) + ".log";
+		final File file=new File(parent, filename);
 		try
 		{
 			/*
@@ -102,24 +102,24 @@ public class UncaughtExceptionHandler implements Thread.UncaughtExceptionHandler
 			 fos.write ( stringWriter.toString ( ).getBytes ( ) );
 			 fos.close ( );
 			 */
-			Sink s=Okio.sink ( file );
-			BufferedSink bs=Okio.buffer ( s );
-			bs.writeUtf8 ( String.format ( "Device:%s %n", Build.DEVICE ) );
-			bs.writeUtf8 ( String.format ( "App ver:%s %n", app_ver ) );
-			bs.writeUtf8 ( String.format ( "App ver_int:%d %n", app_ver_int ) );
-			bs.writeUtf8 ( stringWriter.toString ( ) );
-			bs.flush ( );
-			bs.close ( );
-			s.close ( );
+			Sink s=Okio.sink(file);
+			BufferedSink bs=Okio.buffer(s);
+			bs.writeUtf8(String.format("Device:%s (%s)%n", Build.MODEL, Build.DEVICE));
+			bs.writeUtf8(String.format("App ver:%s %n", app_ver));
+			bs.writeUtf8(String.format("App ver_int:%d %n", app_ver_int));
+			bs.writeUtf8(stringWriter.toString());
+			bs.flush();
+			bs.close();
+			s.close();
 
 
 		}
 		catch (Exception e)
 		{
-			e.printStackTrace ( );
+			e.printStackTrace();
 		}
-		ctx.getSharedPreferences ( PREF_NAME, 0 ).edit ( ).putLong ( ERRTIME, time ).commit ( );
-		android.os.Process.killProcess ( android.os.Process.myPid ( ) );
+		ctx.getSharedPreferences(PREF_NAME, 0).edit().putLong(ERRTIME, time).commit();
+		android.os.Process.killProcess(android.os.Process.myPid());
 		/*F
 		 Disabled for satble reason.
 		 new Thread(){
