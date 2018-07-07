59ab374dbf78d32a0a63777d3b830aab379df99c
CHH2000day <chh1932383611@gmail.com>
2018-07-07 14:20:53 +0800

    Update dependebcies Disabled minify

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/AudioFormatHelper.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/AudioFormatHelper.java
@@ -54,33 +54,33 @@ public class AudioFormatHelper
 	//返回的结果
 	public static final String RESULT_OK="OK";
 
-	public AudioFormatHelper ( File audioFile, Context context ) throws FileNotFoundException
+	public AudioFormatHelper(File audioFile, Context context) throws FileNotFoundException
 	{
 
 		//检查文件是否存在及可读
-		if ( !audioFile.exists ( ) || !audioFile.canRead ( ) )
+		if (!audioFile.exists() || !audioFile.canRead())
 		{
-			throw new FileNotFoundException ( "Audio file:" + audioFile.getPath ( ) + " could not be found" );
+			throw new FileNotFoundException("Audio file:" + audioFile.getPath() + " could not be found");
 		}
-		init ( );
+		init();
 		this.srcFile = audioFile;
 		mcontext = context;
 	}
-	public AudioFormatHelper ( Uri audioFilePath, Context context )
+	public AudioFormatHelper(Uri audioFilePath, Context context)
 	{
-		init ( );
+		init();
 		srcFileUri = audioFilePath;
 		mcontext = context;
 	}
-	private void init ( )
+	private void init()
 	{
-		cachedFiles = new ArrayList<File> ( );
-		valids = new HashMap<File,Boolean> ( );
-		mBufferSize = AudioRecord.getMinBufferSize ( mSampleRate, mChannel, mEncoding );
-		mEmptyHandler = new Handler ( ){
+		cachedFiles = new ArrayList<File>();
+		valids = new HashMap<File,Boolean>();
+		mBufferSize = AudioRecord.getMinBufferSize(mSampleRate, mChannel, mEncoding);
+		mEmptyHandler = new Handler(){
 
 			@Override
-			public void handleMessage ( Message msg )
+			public void handleMessage(Message msg)
 			{
 				// TODO: Implement this method
 				//DO:NOTHING
@@ -89,35 +89,35 @@ public class AudioFormatHelper
 		};
 	}
 	//转码至wav
-	public String compressToWav ( File targetFile )
+	public String compressToWav(File targetFile)
 	{
-		return compressToWav ( targetFile, mEmptyHandler );
+		return compressToWav(targetFile, mEmptyHandler);
 	}
-	public String compressToWav ( final File targetFile, final Handler UIHandler )
+	public String compressToWav(final File targetFile, final Handler UIHandler)
 	{
-		if ( !targetFile.getParentFile ( ).exists ( ) )
+		if (!targetFile.getParentFile().exists())
 		{
-			targetFile.getParentFile ( ).mkdirs ( );
+			targetFile.getParentFile().mkdirs();
 		}
 		String errorcode="";
-		UIHandler.sendEmptyMessage ( STATUS_START );
+		UIHandler.sendEmptyMessage(STATUS_START);
 		InputStream in=null;
-		File cachedFile=getValidCacheFile ( );
-		if ( !isProcessed || cachedFile == null )
+		File cachedFile=getValidCacheFile();
+		if (!isProcessed || cachedFile == null)
 		{	try
 			{
 				//验证源文件是否已为wav格式
-				if ( srcFile != null )
+				if (srcFile != null)
 				{
-					in = new FileInputStream ( srcFile );
+					in = new FileInputStream(srcFile);
 				}
 				else
 				{
-					in = mcontext.getContentResolver ( ).openInputStream ( srcFileUri );
+					in = mcontext.getContentResolver().openInputStream(srcFileUri);
 				}
 				byte[] b=new byte[4];
-				in.read ( b );
-				if ( Arrays.equals ( b, HEADER_WAV ) )
+				in.read(b);
+				if (Arrays.equals(b, HEADER_WAV))
 				{
 
 					//若是，直接读取为已解码数据，并跳过添加文件头
@@ -142,106 +142,108 @@ public class AudioFormatHelper
 				{
 					//解码数据
 					//boolean bl=decodeAudio ( null, UIHandler );
-					decodeAudio ( UIHandler );
+					decodeAudio(UIHandler);
 				}
 			}
 			catch (Exception e)
 			{
-				UIHandler.sendMessage ( UIHandler.obtainMessage ( STATUS_ERROR, e ) );
-				return e.getMessage ( );
+				UIHandler.sendMessage(UIHandler.obtainMessage(STATUS_ERROR, e));
+				return e.getMessage();
 			}
 		}
 		try
 		{
-			UIHandler.sendEmptyMessage ( STATUS_WRITING );
-			FormatHelperFactory.refreshCache ( targetFile );
+			UIHandler.sendEmptyMessage(STATUS_WRITING);
+			FormatHelperFactory.refreshCache(targetFile);
 			//如果存在已处理好的缓存文件，直接复制
-			if ( isProcessed && cachedFile != null && cachedFile.exists ( ) )
+			if (isProcessed && cachedFile != null && cachedFile.exists())
 			{
-				if(!targetFile.getParentFile().exists()){
+				if (!targetFile.getParentFile().exists())
+				{
 					targetFile.getParentFile().mkdirs();
 				}
-				Utils.copyFile ( cachedFile, targetFile );
+				Utils.copyFile(cachedFile, targetFile);
 
 			}
 			else
 			{
-				Sink s=Okio.sink ( targetFile );
-				BufferedSink f=Okio.buffer ( s );
+				Sink s=Okio.sink(targetFile);
+				BufferedSink f=Okio.buffer(s);
 				//添加文件头
 				//若源文件已为wav格式，直接读取源文件并写入
-				if ( isUnneededtocompress )
+				if (isUnneededtocompress)
 				{
-					f.write ( HEADER_WAV );
+					f.write(HEADER_WAV);
 					byte[] cache=new byte[2048];
 					int len;
-					while ( ( len = in.read ( cache ) ) != -1 )
+					while ((len = in.read(cache)) != -1)
 					{
-						f.write ( cache, 0, len );
+						f.write(cache, 0, len);
 					}
 				}
 				else
 				{
 					//从缓存文件读取数据并处理
-					if(cacheFile==null||!cacheFile.exists()){
+					if (cacheFile == null || !cacheFile.exists())
+					{
 						throw new IOException("Unable to raed transcode cache,it may have been deleted by system");
 					}
-					FileInputStream fis=new FileInputStream ( cacheFile );
-					f.write ( getWavHeader ( fis.available ( ) ) );	
+					FileInputStream fis=new FileInputStream(cacheFile);
+					f.write(getWavHeader(fis.available()));	
 					int len;
 					byte[] cache=new byte[2048];
-					while ( ( len = fis.read ( cache ) ) != -1 )
+					while ((len = fis.read(cache)) != -1)
 					{
-						f.write ( cache, 0, len );
+						f.write(cache, 0, len);
 					}
-					fis.close ( );
+					fis.close();
 				}
-				f.flush ( );
-				f.close ( );
-				if ( in != null )
+				f.flush();
+				f.close();
+				if (in != null)
 				{
-					in.close ( );}
-				activeCache ( targetFile );
+					in.close();}
+				activeCache(targetFile);
 			}
 
 		}
 		catch (IOException e)
 		{
-			UIHandler.sendMessage ( UIHandler.obtainMessage ( STATUS_ERROR, e ) );
-			return e.getMessage ( );
+			UIHandler.sendMessage(UIHandler.obtainMessage(STATUS_ERROR, e));
+			return e.getMessage();
 		}
 		errorcode = RESULT_OK;
-		UIHandler.sendEmptyMessage ( STATUS_DONE );
+		UIHandler.sendEmptyMessage(STATUS_DONE);
 		isProcessed = true;
 		return errorcode;
 
 	}
-	private boolean decodeAudio (/* String[] exceptions ,*/ final Handler UIHandler ) throws Exception
+	private boolean decodeAudio(final Handler UIHandler) throws Exception
 	{
 
-		UIHandler.sendEmptyMessage ( STATUS_LOADINGFILE );
+		UIHandler.sendEmptyMessage(STATUS_LOADINGFILE);
 		//配置音轨分离器
 		//decodeddata = new ByteArrayOutputStream ( );
-		final MediaExtractor me=new MediaExtractor ( );
-		cacheFile = new File ( mcontext.getCacheDir ( ), "cache.pcm" );
+		final MediaExtractor me=new MediaExtractor();
+		cacheFile = new File(mcontext.getCacheDir(), "cache.pcm");
 		//配置数据源，默认优先使用File
-		if ( srcFile != null )
+		if (srcFile != null)
 		{
-			me.setDataSource ( srcFile.getAbsolutePath ( ) );
+			me.setDataSource(srcFile.getAbsolutePath());
 		}
 		else
 		{
-			me.setDataSource ( mcontext, srcFileUri, null );
+			me.setDataSource(mcontext, srcFileUri, null);
 		}
 		int soundtrackIndex=-1;
 		String mime="";
 		MediaFormat md=null;
 		//获取音频轨
-		for ( int i=0;i < me.getTrackCount ( );i++ )
+		for (int i=0;i < me.getTrackCount();i++)
 		{
-			md = me.getTrackFormat ( i );
+			md = me.getTrackFormat(i);
 			//通过检验mime判断是否为音频轨
-			if ( ( mime = md.getString ( MediaFormat.KEY_MIME ) ).startsWith ( "audio" ) )
+			if ((mime = md.getString(MediaFormat.KEY_MIME)).startsWith("audio"))
 			{//若为音频轨，判断是否在需要跳过解码的例外中
 				//
 				//取消例外在解码过程中的处理，转为在处理时处理
@@ -271,61 +273,61 @@ public class AudioFormatHelper
 				//更新选定的音轨
 				soundtrackIndex = i;
 				//从文件获取采样率与声道数
-				mSampleRate = md.getInteger ( MediaFormat.KEY_SAMPLE_RATE );
-				mChannel = md.getInteger ( MediaFormat.KEY_CHANNEL_COUNT );
+				mSampleRate = md.getInteger(MediaFormat.KEY_SAMPLE_RATE);
+				mChannel = md.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
 				break;
 			}
 		}
-		if ( soundtrackIndex == -1 )
+		if (soundtrackIndex == -1)
 		{
-			throw new Exception ( "No audio track counld be found from file." );
+			throw new Exception("No audio track counld be found from file.");
 		}
-		me.selectTrack ( soundtrackIndex );
+		me.selectTrack(soundtrackIndex);
 		//创建解码器
-		MediaCodec mc=MediaCodec.createDecoderByType ( mime );
-		mc.configure ( md, null, null, 0 );
-		UIHandler.sendEmptyMessage ( STATUS_TRANSCODING );
+		MediaCodec mc=MediaCodec.createDecoderByType(mime);
+		mc.configure(md, null, null, 0);
+		UIHandler.sendEmptyMessage(STATUS_TRANSCODING);
 
 		//创建缓存文件的输出流
-		if ( !cacheFile.getParentFile ( ).exists ( ) )
+		if (!cacheFile.getParentFile().exists())
 		{
-			cacheFile.getParentFile ( ).mkdirs ( );
+			cacheFile.getParentFile().mkdirs();
 		}
-		final FileOutputStream fos=new FileOutputStream ( cacheFile );
-		mc.setCallback ( new MediaCodec.Callback ( ){
+		final FileOutputStream fos=new FileOutputStream(cacheFile);
+		mc.setCallback(new MediaCodec.Callback(){
 
 				int role=0;
 				@Override
-				public void onInputBufferAvailable ( MediaCodec p1, int p2 )
+				public void onInputBufferAvailable(MediaCodec p1, int p2)
 				{
 					//如果解码完成，直接停止输入数据
-					if ( isdecoded )
+					if (isdecoded)
 					{return;}
 					//role:输入次数
 					role++;
-					int len=me.readSampleData ( p1.getInputBuffer ( p2 ), 0 );
-					if ( len < 0 )
+					int len=me.readSampleData(p1.getInputBuffer(p2), 0);
+					if (len < 0)
 					{//如果数据读完，通知解码器
-						p1.queueInputBuffer ( p2, 0, 0, 0, p1.BUFFER_FLAG_END_OF_STREAM );
+						p1.queueInputBuffer(p2, 0, 0, 0, p1.BUFFER_FLAG_END_OF_STREAM);
 					}
 					else
 					{
-						p1.queueInputBuffer ( p2, 0, len, 0, 0 );
+						p1.queueInputBuffer(p2, 0, len, 0, 0);
 					}
 					//读取完数据后，移入下一帧
-					me.advance ( );
+					me.advance();
 
 
 				}
 
 				@Override
-				public void onOutputBufferAvailable ( MediaCodec p1, int p2, MediaCodec.BufferInfo p3 )
+				public void onOutputBufferAvailable(MediaCodec p1, int p2, MediaCodec.BufferInfo p3)
 				{
-					if ( p3.flags == p1.BUFFER_FLAG_END_OF_STREAM )
+					if (p3.flags == p1.BUFFER_FLAG_END_OF_STREAM)
 					{//如果解码器提示数据读完，停止输入数据，关闭输出流，通知主线程
 						try
 						{
-							fos.close ( );
+							fos.close();
 						}
 						catch (IOException e)
 						{//忽略
@@ -336,10 +338,10 @@ public class AudioFormatHelper
 					else
 					{
 						//从解码器读取数据
-						byte[] b=new byte[p1.getOutputBuffer ( p2 ).remaining ( )];
-						p1.getOutputBuffer ( p2 ).get ( b, 0, b.length );
-						p1.getOutputBuffer ( p2 ).clear ( );
-						p1.releaseOutputBuffer ( p2, false );
+						byte[] b=new byte[p1.getOutputBuffer(p2).remaining()];
+						p1.getOutputBuffer(p2).get(b, 0, b.length);
+						p1.getOutputBuffer(p2).clear();
+						p1.releaseOutputBuffer(p2, false);
 						/*
 						 //将数据写入ByteStream
 						 //decodeddata.write ( b, 0, b.length );
@@ -347,11 +349,11 @@ public class AudioFormatHelper
 						//将解码后数据写入本地缓存
 						try
 						{
-							fos.write ( b, 0, b.length );
+							fos.write(b, 0, b.length);
 						}
 						catch (IOException e)
 						{
-							UIHandler.sendMessage ( UIHandler.obtainMessage ( STATUS_ERROR, e ) );
+							UIHandler.sendMessage(UIHandler.obtainMessage(STATUS_ERROR, e));
 						}
 
 					}
@@ -361,35 +363,35 @@ public class AudioFormatHelper
 				}
 
 				@Override
-				public void onError ( MediaCodec p1, MediaCodec.CodecException p2 )
+				public void onError(MediaCodec p1, MediaCodec.CodecException p2)
 				{
-					UIHandler.handleMessage ( UIHandler.obtainMessage ( STATUS_ERROR, p2 ) );
+					UIHandler.handleMessage(UIHandler.obtainMessage(STATUS_ERROR, p2));
 					// TODO: Implement this method
 				}
 
 				@Override
-				public void onOutputFormatChanged ( MediaCodec p1, MediaFormat p2 )
+				public void onOutputFormatChanged(MediaCodec p1, MediaFormat p2)
 				{
 
 					// TODO: Implement this method
 				}
-			} );
+			});
 		//启动解码器，直至解码完成
-		mc.start ( );
+		mc.start();
 		do{
 			try
-			{Thread.sleep ( 50 );}
+			{Thread.sleep(50);}
 			finally
 			{}
 		}while(!isdecoded);
 		//解码结束，释放资源
-		mc.stop ( );
-		mc.release ( );
-		me.release ( );
+		mc.stop();
+		mc.release();
+		me.release();
 		return false;
 	}
 	//由pcm格式数据获取wav的文件头
-	private byte[] getWavHeader ( long audioLength )
+	private byte[] getWavHeader(long audioLength)
 	{
 		long totalDataLength=audioLength + 8;
 		long byteRate = 16 * mSampleRate * mChannel / 8;
@@ -399,112 +401,112 @@ public class AudioFormatHelper
 		/*不用arraycopy防止阅读错误
 		 System.arraycopy(HEADER_WAV,0,header,0,4);
 		 */
-		header [ 0 ] = HEADER_WAV [ 0 ];
-		header [ 1 ] = HEADER_WAV [ 1 ];
-		header [ 2 ] = HEADER_WAV [ 2 ];
-		header [ 3 ] = HEADER_WAV [ 3 ];
+		header[0] = HEADER_WAV[0];
+		header[1] = HEADER_WAV[1];
+		header[2] = HEADER_WAV[2];
+		header[3] = HEADER_WAV[3];
 		//载入文件总长度数据
-		header [ 4 ] = (byte)( totalDataLength & 0xFF );
-		header [ 5 ] = (byte)( ( totalDataLength >> 8 ) & 0xFF );
-		header [ 6 ] = (byte)( ( totalDataLength >> 16 ) & 0xFF );
-		header [ 7 ] = (byte)( ( totalDataLength >> 24 ) & 0xFF );
-		header [ 8 ] = 'W';
-		header [ 9 ] = 'A';
-		header [ 10 ] = 'V';
-		header [ 11 ] = 'E';
+		header[4] = (byte)(totalDataLength & 0xFF);
+		header[5] = (byte)((totalDataLength >> 8) & 0xFF);
+		header[6] = (byte)((totalDataLength >> 16) & 0xFF);
+		header[7] = (byte)((totalDataLength >> 24) & 0xFF);
+		header[8] = 'W';
+		header[9] = 'A';
+		header[10] = 'V';
+		header[11] = 'E';
 		//文件格式format chunk
-		header [ 12 ] = 'f';
-		header [ 13 ] = 'm';
-		header [ 14 ] = 't';
-		header [ 15 ] = ' ';
-		header [ 16 ] = 16;//(16-19)size:16 无附加信息
-		header [ 17 ] = 0;
-		header [ 18 ] = 0;
-		header [ 19 ] = 0;
-		header [ 20 ] = 1;//(20-21)编码方式0x0001
-		header [ 21 ] = 0;
-		header [ 22 ] = (byte)mChannel;//(22-23)声道数 2:立体声 1:单声道
-		header [ 23 ] = 0;
-		header [ 24 ] = (byte)( mSampleRate & 0xFF );
-		header [ 25 ] = (byte)( ( mSampleRate >> 8 ) & 0xFF );
-		header [ 26 ] = (byte)( ( mSampleRate >> 16 ) & 0xFF );
-		header [ 27 ] = (byte)( ( mSampleRate >> 24 ) & 0xFF );//(24-27)采样率
-		header [ 28 ] = (byte)( byteRate & 0xFF );
-		header [ 29 ] = (byte)( ( byteRate >> 8 ) & 0xFF );
-		header [ 30 ] = (byte)( ( byteRate >> 16 ) & 0xFF );
-		header [ 31 ] = (byte)( ( byteRate >> 24 ) & 0xFF );//(28-31)每秒所需字节数
-		header [ 32 ] = 2 * 16 / 8;
-		header [ 33 ] = 0;//(32-33) 每个采样所需字节数
-		header [ 34 ] = 16;
-		header [ 35 ] = 0;//(34-35) 每采样需要的bit
+		header[12] = 'f';
+		header[13] = 'm';
+		header[14] = 't';
+		header[15] = ' ';
+		header[16] = 16;//(16-19)size:16 无附加信息
+		header[17] = 0;
+		header[18] = 0;
+		header[19] = 0;
+		header[20] = 1;//(20-21)编码方式0x0001
+		header[21] = 0;
+		header[22] = (byte)mChannel;//(22-23)声道数 2:立体声 1:单声道
+		header[23] = 0;
+		header[24] = (byte)(mSampleRate & 0xFF);
+		header[25] = (byte)((mSampleRate >> 8) & 0xFF);
+		header[26] = (byte)((mSampleRate >> 16) & 0xFF);
+		header[27] = (byte)((mSampleRate >> 24) & 0xFF);//(24-27)采样率
+		header[28] = (byte)(byteRate & 0xFF);
+		header[29] = (byte)((byteRate >> 8) & 0xFF);
+		header[30] = (byte)((byteRate >> 16) & 0xFF);
+		header[31] = (byte)((byteRate >> 24) & 0xFF);//(28-31)每秒所需字节数
+		header[32] = 2 * 16 / 8;
+		header[33] = 0;//(32-33) 每个采样所需字节数
+		header[34] = 16;
+		header[35] = 0;//(34-35) 每采样需要的bit
 		//data chunk
-		header [ 36 ] = 'd';
-		header [ 37 ] = 'a';
-		header [ 38 ] = 't';
-		header [ 39 ] = 'a';
-		header [ 40 ] = (byte)( audioLength & 0xFF );
-		header [ 41 ] = (byte)( ( audioLength >> 8 ) & 0xFF );
-		header [ 42 ] = (byte)( ( audioLength >> 16 ) & 0xFF );
-		header [ 43 ] = (byte)( ( audioLength >> 24 ) & 0xFF );//(40-43)数据长度
+		header[36] = 'd';
+		header[37] = 'a';
+		header[38] = 't';
+		header[39] = 'a';
+		header[40] = (byte)(audioLength & 0xFF);
+		header[41] = (byte)((audioLength >> 8) & 0xFF);
+		header[42] = (byte)((audioLength >> 16) & 0xFF);
+		header[43] = (byte)((audioLength >> 24) & 0xFF);//(40-43)数据长度
 		//文件头生成完成
 		return header;
 	}
 
 
 	//回收使用后的临时资源
-	public void recycle ( )
+	public void recycle()
 	{
 		/*if ( decodeddata != null )
 		 {
 		 decodeddata.reset ( );}*/
-		if ( cacheFile != null )
+		if (cacheFile != null)
 		{
-			cacheFile.delete ( );
+			cacheFile.delete();
 		}
 		isdecoded = false;
 	}
 
 	//使缓存失效
-	public synchronized void denyCache ( File file, int mode )
+	public synchronized void denyCache(File file, int mode)
 	{
-		if ( MODE_DENY_ALL_CACHE == mode )
+		if (MODE_DENY_ALL_CACHE == mode)
 		{
-			valids.clear ( );
+			valids.clear();
 		}
 		else
 		{
-			if ( file != null )
+			if (file != null)
 			{
-				if ( cachedFiles.contains ( file ) )
+				if (cachedFiles.contains(file))
 				{
-					valids.put ( file, new Boolean ( false ) );
+					valids.put(file, new Boolean(false));
 				}
 			}
 		}
 	}
-	public void denyCache ( File file )
+	public void denyCache(File file)
 	{
-		denyCache ( file, 0 );
+		denyCache(file, 0);
 	}
 
 	//激活已转码为wav的缓存文件
-	protected synchronized void activeCache ( File file )
+	protected synchronized void activeCache(File file)
 	{
-		if ( !cachedFiles.contains ( file ) )
+		if (!cachedFiles.contains(file))
 		{
-			cachedFiles.add ( file );
+			cachedFiles.add(file);
 		}
-		valids.put ( file, new Boolean ( true ) );
+		valids.put(file, new Boolean(true));
 	}
-	private synchronized File getValidCacheFile ( )
+	private synchronized File getValidCacheFile()
 	{
-		if ( cachedFiles.isEmpty ( ) || valids.isEmpty ( )  )
+		if (cachedFiles.isEmpty() || valids.isEmpty())
 		{
 			return null;
 		}
-		for ( File f:cachedFiles )
+		for (File f:cachedFiles)
 		{
-			if ( ( valids.get ( f ) != null ) && valids.get ( f ) )
+			if ((valids.get(f) != null) && valids.get(f))
 			{
 				return f;
 			}
