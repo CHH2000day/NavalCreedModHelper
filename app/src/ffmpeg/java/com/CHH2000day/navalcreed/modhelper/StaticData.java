c24567ad27d8e347201bd5a6ae391d9baec9d980
CHH2000day <chh1932383611@gmail.com>
2018-12-02 11:10:51 +0800

    Separated update chennal for ffmpeg version and common version

---
+++ app/src/ffmpeg/java/com/CHH2000day/navalcreed/modhelper/StaticData.java
@@ -0,0 +1,23 @@
+package com.CHH2000day.navalcreed.modhelper;
+
+public class StaticData
+{
+	public static final String API_KEY="1aea5425cdef9a5a7a17cafcd8df925b";
+	public static final String DATAID_ALPHA="tvUr2228";
+	public static final String DATAID_RELEASE="JNWw5557";
+	public static final String DATAID=BuildConfig.DEBUG?DATAID_ALPHA:DATAID_RELEASE;
+	public static final String DATA_ID_ANTIHEXIE="945119b17f";
+	//public static final String DATA_ID_ANNOUNCEMENT="ZeHyBBBP";
+	private static final String DATA_ID_ANNOUNCEMENT_ALPHA="fb6aec3eec";
+	private static final String DATA_ID_ANNOUNCEMENT_RELEASE="6aff9eb4d9";
+	public static final String DATA_ID_ANNOUNCEMENT=BuildConfig.DEBUG ? DATA_ID_ANNOUNCEMENT_ALPHA: DATA_ID_ANNOUNCEMENT_RELEASE;
+	public static final String KEY_TESTER_ID="Testet_id";
+	/*public static final String getDataid ( )
+	{
+		if ( BuildConfig.DEBUG )
+		{
+			return DATAID_ALPHA;
+		}
+		return DATAID_RELEASE;
+	}*/
+}
