fb18c5e29a8396a0429e83ff58185b47ceaa4a60
CHH2000day <chh1932383611@gmail.com>
2019-05-09 19:42:33 +0800

    Build 2019050900 (10003)

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/ModHelperApplication.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/ModHelperApplication.java
@@ -15,7 +15,7 @@ import com.orhanobut.logger.*;
 import com.tencent.bugly.crashreport.*;
 public class ModHelperApplication extends Application
 {
-	public static final int BUILDVER=10002;
+	public static final int BUILDVER=10003;
 	//never used
 	//private android.os.Handler merrmsghdl;
 	private File resDir;
