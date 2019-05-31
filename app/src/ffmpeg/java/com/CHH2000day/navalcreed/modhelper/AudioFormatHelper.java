d7b8518eda9760bdd1ceb0585548da92ae52721f
CHH2000day <chh1932383611@gmail.com>
2019-05-31 22:24:35 +0800

    Fix wrong arg

--- app/src/ffmpeg/java/com/CHH2000day/navalcreed/modhelper/AudioFormatHelper.java
+++ app/src/ffmpeg/java/com/CHH2000day/navalcreed/modhelper/AudioFormatHelper.java
@@ -165,7 +165,7 @@ public class AudioFormatHelper
 							public void onFailure(String p1)
 							{
 								hasError = true;
-								UIHandler.sendEmptyMessage(STATUS_ERROR,new IOException(p1));
+								UIHandler.sendMessage(UIHandler.obtainMessage(STATUS_ERROR, new IOException(p1)));
 								// TODO: Implement this method
 							}
 
