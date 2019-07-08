bbafcbb9a10bd5453c58923a080c9636c68aaf72
CHH2000day <chh1932383611@gmail.com>
2019-07-08 08:09:10 +0000

    Fix ad loading

--- app/src/main/java/com/CHH2000day/navalcreed/modhelper/AboutFragment.java
+++ app/src/main/java/com/CHH2000day/navalcreed/modhelper/AboutFragment.java
@@ -60,6 +60,7 @@ public class AboutFragment extends Fragment {
                         ad.show();
                     }
                 });
+		ad.load();
                 return true;
             });
             donate.setOnClickListener(p1 -> {
